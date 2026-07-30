2e575e6cfa9228ea8d0feac801dfca7e506f5446
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-07-30 21:55:57 +0530

    feat(ambient): Add pink noise generator stop method

--- src/utils/ambientAudio.js
+++ src/utils/ambientAudio.js
@@ -4,3 +4,5 @@ export function startWhiteNoise() { console.log('White noise started'); }
 export function stopWhiteNoise() { console.log('White noise stopped'); }
 
 export function startPinkNoise() { console.log('Pink noise started'); }
+
+export function stopPinkNoise() { console.log('Pink noise stopped'); }
