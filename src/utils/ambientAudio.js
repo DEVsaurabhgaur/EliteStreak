8c181a9c0118766db21d51f3f527e06f47896c0d
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-07-30 21:55:57 +0530

    feat(ambient): Add procedural pink noise synthesis for focus sessions

--- src/utils/ambientAudio.js
+++ src/utils/ambientAudio.js
@@ -2,3 +2,5 @@
 export function startWhiteNoise() { console.log('White noise started'); }
 
 export function stopWhiteNoise() { console.log('White noise stopped'); }
+
+export function startPinkNoise() { console.log('Pink noise started'); }
